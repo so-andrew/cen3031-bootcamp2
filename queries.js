84792a2fd493bf28cb44afe6141e6da9b61689c0
Andrew So <so.andrew@ufl.edu>
2019-09-06 13:48:34 -0400

    Initial commit

--- queries.js
+++ queries.js
@@ -1,15 +1,26 @@
 /* Add all the required libraries*/
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
+    config = require('./config');
 
 /* Connect to your database using mongoose - remember to keep your key secret*/
-
-/* Fill out these functions using Mongoose queries*/
-//Check out - https://mongoosejs.com/docs/queries.html
+mongoose.connect(config.db.uri, {useNewUrlParser: true}, function(err){
+    if(err) throw err;
+});
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
 
 var findLibraryWest = function() {
   /* 
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+  Listing.findOne({ 'name': 'Library West' }, function(err, listing){
+      if(err) throw err;
+      console.log(listing);
+  });
+
 };
 var removeCable = function() {
   /*
@@ -17,17 +28,29 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+  Listing.findOneAndDelete({ 'code': 'CABL'}, function(err, listing){
+      if(err) throw err;
+      console.log(listing);
+  });
 };
 var updatePhelpsMemorial = function() {
   /*
     Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+  Listing.findOneAndUpdate({ 'code': 'PHL' }, { 'address': '1953 Museum Rd, Gainesville, FL 32603, United States' }, { 'new': true }, function(err, listing){
+      if(err) throw err;
+      console.log(listing);
+  });
 };
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+  Listing.find({}, function(err, listings){
+      if(err) throw err;
+      console.log(listings);
+  });
 };
 
 findLibraryWest();
